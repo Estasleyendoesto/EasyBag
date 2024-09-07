af2bb507a583a878c636807b3f58aafa8dc1883f
Estasleyendoesto <alsival@hotmail.com>
2024-09-08 01:48:44 +0200

    El primer

---
+++ localization.lua
@@ -0,0 +1,33 @@
+EasyBag = EasyBag or {}
+EasyBag.LOCALIZATION = {
+    ["esES"] = {
+        {text = "Todo",                                          typo = "Todo"},
+        {text = "Llaves",                                        typo = "Miscelánea", subtype= "Llave"},
+        {text = "Cofres",                                        typo = "Miscelánea", subtype= "Contenedor"},
+        {text = "Objetos de Misión",                             typo = "Miscelánea", subtype= "Misión"},
+        {text = "Coleccionables (Monturas, Mascotas, Juguetes)", typo = "Miscelánea"},
+        {text = "Materiales de Profesión",                       typo = "Habilidad Comercial"},
+        {text = "Armaduras",                                     typo = "Armadura"},
+        {text = "Armas",                                         typo = "Arma"},
+        {text = "Anillos y amuletos",                            typo = "Armadura"},
+        {text = "Abalorios",                                     typo = "Armadura"},
+        {text = "Consumibles",                                   typo = "Consumible"},
+        {text = "Recetas",                                       typo = "Receta"},
+        ["tooltip"] = "Filtrar"
+    },
+    ["enUS"] = {
+        {text = "All",                               type = ""},
+        {text = "Keys",                              type = "", subtype= ""},
+        {text = "Chests",                            type = "", subtype= ""},
+        {text = "Quest Items",                       type = "", subtype= ""},
+        {text = "Collectibles (Mounts, Pets, Toys)", typo = ""},
+        {text = "Materials (Profession)",            type = ""},
+        {text = "Armors",                            type = ""},
+        {text = "Weapons",                           type = ""},
+        {text = "Rings and Amulets",                 type = ""},
+        {text = "Trinkets",                          type = ""},
+        {text = "Consumables (Potions, Food, ...)",  type = ""},
+        {text = "Recipes",                           type = ""},
+        ["tooltip"] = "Filter By"
+    }
+}
